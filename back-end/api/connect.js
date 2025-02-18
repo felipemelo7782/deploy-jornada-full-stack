ea2aeceecdd39205614bf7ce7040c8ebdddcb551
felipemelo7782 <FG203253@GMAIL.COM>
2025-02-18 13:41:16 -0300

    finalizamos o projeto

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -1,10 +1,14 @@
 // JavaScript Assincrono
 // await async
 // Fullfilled
+
 import { MongoClient } from "mongodb";
 
+ const passwordCluster = "Hl8CzGHk3fHOFFbq"
+
 const URI =
-  "mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+`mongodb+srv://dg203253:${passwordCluster}@cluster0.nospj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+//"mongodb+srv://fullstackjornada:qojI71xVU2aV8UKC@cluster0.v1qra.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 const client = new MongoClient(URI);
 
